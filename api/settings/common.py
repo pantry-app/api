e4d253e064d0971c4e5f1e4b6aa628cab478c986
Tristan K <tristanmkernan@gmail.com>
2019-09-06 15:12:28 -0400

    fix: enable cors via django cors headers

--- api/settings/common.py
+++ api/settings/common.py
@@ -36,6 +36,7 @@ INSTALLED_APPS = [
     "djoser",
     "django_filters",
     "django_extensions",
+    "corsheaders",
     "account.apps.AccountConfig",
     "pantry.apps.PantryConfig",
     "shoppinglist.apps.ShoppinglistConfig",
@@ -45,6 +46,7 @@ INSTALLED_APPS = [
 MIDDLEWARE = [
     "django.middleware.security.SecurityMiddleware",
     "django.contrib.sessions.middleware.SessionMiddleware",
+    "corsheaders.middleware.CorsMiddleware",
     "django.middleware.common.CommonMiddleware",
     "django.middleware.csrf.CsrfViewMiddleware",
     "django.contrib.auth.middleware.AuthenticationMiddleware",
